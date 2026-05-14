8507389055e7891dad6168ec3afac91f67bef668
Enes Dinç <enes47794@gmail.com>
2026-05-14 20:55:39 +0300

    Create biyokaynakca.ts

---
+++ src/data/biyokaynakca.ts
@@ -0,0 +1,15 @@
+const biyolojiKaynakca = [
+  {
+    title: 'Genetik ve Evrim',
+    url: 'https://evrimagaci.org/k/genetik/',
+    description: 'Genetik ve evrim ile ilgili ayrıntılı kaynak.'
+  },
+  {
+    title: 'Khan Academy Biyoloji',
+    url: 'https://tr.khanacademy.org/science/biology',
+    description: 'Khan Academy\'de biyoloji eğitimleri.'
+  },
+  // Buraya yeni kaynak eklemek için diziye yeni obje ekle
+];
+
+export default biyolojiKaynakca;
